92ee12b4a465b3bbd945646bebb3a90b71ac6c45
Eylen <saioapicado@gmail.com>
2022-02-19 11:23:36 +0100

    milestone1 chore: render DataFetchingContainer to test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import DashboardShell from "./features/Dashboard/DashboardShell";
+import DataFetchingContainer from './features/Dashboard/DataFetchingContainer';
+import DashboardShell from './features/Dashboard/DashboardShell';
 
 const App = () => {
-  return <DashboardShell />;
+  return <DataFetchingContainer />;
 };
 
 export default App;
