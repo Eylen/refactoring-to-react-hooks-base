5a77d155699c2fcd70b5af65bece76a4e289d857
Eylen <saioapicado@gmail.com>
2022-02-19 10:59:28 +0100

    milestone1 feat(datafetchingcontainer): add DataFetchingContainer

---
+++ src/features/Dashboard/DataFetchingContainer.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import DataFetching from '../../common/components/DataFetching';
+import Select from '../../common/components/Select';
+
+const DataFetchingContainer = () => {
+  const [selectedEndpoint, setSelectedEnpoint] = useState('totals');
+
+  const optionsForSelect = [
+    {label: '--', value: 'totals'},
+    {label: 'Sales', value: 'sales'},
+    {label: 'Subscriptions', value: 'subscriptions'}
+  ];
+
+  const handleSelectChange = (event) => {
+    setSelectedEnpoint(
+      `${process.env.REACT_APP_BASE_URL}/${event.target.value}`
+    );
+  };
+
+  return (
+    <>
+      <Select
+        onChange={handleSelectChange}
+        label="Please, select a chart"
+        id="select-chart"
+        name="select-chart"
+        options={optionsForSelect}
+      />
+      {selectedEndpoint ? <DataFetching endpoint={selectedEndpoint} /> : null}
+    </>
+  );
+};
+
+export default DataFetchingContainer;
