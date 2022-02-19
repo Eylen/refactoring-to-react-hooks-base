a62c578af8aadec605b55cffbf22150305eab82b
Eylen <saioapicado@gmail.com>
2022-02-19 11:26:46 +0100

    milestone1 fix(select): add label

--- src/common/components/Select.js
+++ src/common/components/Select.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Select = ({className, id, name, value, options, onChange}) => (
-  <select
-    id={id}
-    name={name}
-    className={className}
-    onChange={onChange}
-    value={value}
-  >
-    {options.map(({label, value}) => (
-      <option key={value} value={value}>
-        {label}
-      </option>
-    ))}
-  </select>
+const Select = ({className, id, label, name, value, options, onChange}) => (
+  <>
+    <label htmlFor={id}>{label}</label>
+    <select
+      id={id}
+      name={name}
+      className={className}
+      onChange={onChange}
+      value={value}
+    >
+      {options.map(({label, value}) => (
+        <option key={value} value={value}>
+          {label}
+        </option>
+      ))}
+    </select>
+  </>
 );
 
 Select.propTypes = {
   id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   className: PropTypes.string,
   options: PropTypes.arrayOf(
